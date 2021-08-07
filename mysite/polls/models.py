98118ea5f47f5b71193b357ac9d7d0308b64e69c
myheroyuki <myheroyuki@outlook.com>
2021-08-07 15:44:28 +0900

    More work on Django tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -6,6 +8,10 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 
 class Choice(models.Model):
